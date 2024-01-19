0292f94f688404dfbd34151ae159cc6bb57e220a
rasbin <rasbingodarthapa@gmail.com>
2024-01-19 08:27:17 +0545

    updated sorted

--- sorted_demo.py
+++ sorted_demo.py
@@ -1,10 +1,12 @@
 """ sorted() method """
 
+
 demo_list = [2, -1, -2, 3, -3, 8, 6, 6, 1, 0]
+
+print(sorted(demo_list)) # simple sorted()
 
 
 def sorted_key(num):
     return num * num
 
-
-print(sorted(demo_list, key=sorted_key))
+print(sorted(demo_list, key=sorted_key)) # sorted with key
